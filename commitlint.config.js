3a67f640fabae796b0eb41661588fb3937c10075
Sezai Emre Konuk <sezaikonuk@hacettepe.edu.tr>
2026-08-03 14:05:03 +0300

    feat(commitlint): add ignore rule for "Potential fix for" messages

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,5 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  // GitHub UI "Commit suggestion" (Copilot Autofix) writes its own subject; we can't edit it.
+  ignores: [(message) => /^Potential fix for /.test(message)],
+};
